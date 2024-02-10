aa3166bf5950006467d2ca9590cdafed1b12191e
Ghupe <ghukasyanpetr98@icloud.com>
2024-02-10 13:15:08 +0400

    Pig Game V1.2

--- script.js
+++ script.js
@@ -1,49 +1,50 @@
-const player = document.querySelector('.player')
+'use strict';
 const player1 = document.querySelector('.player--0');
 const player2 = document.querySelector('.player--1');
-const score = document.querySelector(".score")
 const player1Score = document.querySelector('#score--0');
 const player2Score = document.querySelector('#score--1');
 const diceImg = document.querySelector('.dice');
-const currentScore = document.querySelector('.current-score');
-const player1CurrentScoreTxt = document.querySelector('#current--0')
-const player2CurrentScoreTxt = document.querySelector('#current--1')
+const player1CurrentScoreTxt = document.querySelector('#current--0');
+const player2CurrentScoreTxt = document.querySelector('#current--1');
 const newGameBtn = document.querySelector('.btn--new');
 const diceRollBtn = document.querySelector('.btn--roll');
 const holdScoreBtn = document.querySelector('.btn--hold');
-let sumOfDice = 0
-let activePlayer
-let player1currentScore = 0
-let player2currentScore = 0
+const hideMainWindow = document.querySelector('.hide')
+const player1result = document.querySelector('.player1_img')
+const player2result = document.querySelector('.player2_img')
+let sumOfDice = 0;
+let activePlayer;
+let player1currentScore = 0;
+let player2currentScore = 0;
 
 
-if (player1.classList.contains('player--active')){
-  activePlayer = player1
-}else {
-  activePlayer = player2
+if (player1.classList.contains('player--active')) {
+  activePlayer = player1;
+} else {
+  activePlayer = player2;
 }
 
-function scoreClean () {
-  player1Score.textContent = "0"
-  player2Score.textContent = "0"
-  sumOfDice = 0
+function scoreClean() {
+  player1Score.textContent = '0';
+  player2Score.textContent = '0';
+  sumOfDice = 0;
 }
 
-function sideChange (){
+function sideChange() {
 
-  if (activePlayer === player1){
+  if (activePlayer === player1) {
 
-    player1.classList.remove('player--active')
-    player2.classList.add('player--active')
-    activePlayer = player2
-    scoreClean()
+    player1.classList.remove('player--active');
+    player2.classList.add('player--active');
+    activePlayer = player2;
+    scoreClean();
 
-  }else if (activePlayer === player2) {
+  } else if (activePlayer === player2) {
 
-    player2.classList.remove('player--active')
-    player1.classList.add('player--active')
-    activePlayer = player1
-    scoreClean()
+    player2.classList.remove('player--active');
+    player1.classList.add('player--active');
+    activePlayer = player1;
+    scoreClean();
 
   }
 
@@ -56,46 +57,75 @@ function rollTheDice() {
 
   diceImg.src = `dice${diceNum}.png`;
 
-    if (activePlayer === player1 && diceNum !== 1) {
+  if (activePlayer === player1 && diceNum !== 1) {
 
-      sumOfDice += diceNum
-      player1Score.textContent = sumOfDice
+    sumOfDice += diceNum;
+    player1Score.textContent = sumOfDice;
 
-    }else if (activePlayer === player2 && diceNum !== 1) {
 
-      sumOfDice += diceNum
-      player2Score.textContent = sumOfDice
+  } else if (activePlayer === player2 && diceNum !== 1) {
 
-    }else {
+    sumOfDice += diceNum;
+    player2Score.textContent = sumOfDice;
 
-      scoreClean()
-      sideChange()
+  } else {
 
-    }
+    scoreClean();
+    sideChange();
+
+  }
 }
 
 
-function holdScore () {
+function holdScore() {
 
-  if (activePlayer === player1){
+  if (activePlayer === player1) {
 
-    player1currentScore += sumOfDice
-    player1CurrentScoreTxt.textContent = player1currentScore
-    scoreClean()
-    sideChange()
+    player1currentScore += sumOfDice;
+    player1CurrentScoreTxt.textContent = player1currentScore;
+    scoreClean();
+    sideChange();
+    win();
+    console.log(player1currentScore);
 
-  }else if (activePlayer === player2){
+  } else if (activePlayer === player2) {
 
-    player2currentScore += sumOfDice
-    player2CurrentScoreTxt.textContent = player2currentScore
-    scoreClean()
-    sideChange()
+    player2currentScore += sumOfDice;
+    player2CurrentScoreTxt.textContent = player2currentScore;
+    console.log(player2currentScore);
+    win();
+    scoreClean();
+    sideChange();
+
+  }
+}
+
+
+function win() {
+
+  if (player1currentScore >= 20) {
+
+    hideMainWindow.style.display = "none"
+    player1result.src = 'winner.jpeg'
+    player2result.src = 'loser.jpeg'
+    player1result.style.display = "block"
+    player2result.style.display = "block"
+
+
+
+  }else if (player2currentScore >= 20){
+
+    hideMainWindow.style.display = "none"
+    player2result.src = 'winner.jpeg'
+    player1result.src = 'loser.jpeg'
+    player1result.style.display = "block"
+    player2result.style.display = "block"
+
 
   }
 
 }
 
 
-
 diceRollBtn.addEventListener('click', rollTheDice);
-holdScoreBtn.addEventListener('click',holdScore)
+holdScoreBtn.addEventListener('click', holdScore);
